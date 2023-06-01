63211baae3d81507f7b8b4ce0e2433223b739d8b
Antonio Fanoel <antonio.cabral@estudante.ifto.edu.br>
2023-06-01 01:41:12 -0300

    versao3

--- sgpapp/views.py
+++ sgpapp/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
 
 
 def index(request):
     return render(request, 'sgp/index.html')
 
 
-def cadastro(request):
-    return render(request, 'sgp/cadastro.html')
-
-
 def gerenciar(request):
     return render(request, 'sgp/gerenciar.html')
-# Create your views here.
+
+
+def signup(request):
+    form = SignUpForm(request.POST)
+    if request.method == 'POST':
+        form = SignUpForm(request.POST, request.FILES)
+        if form.is_valid():
+            obj = form.save()
+            obj.save()
+            form = SignUpForm()
+    return render(request, 'sgp/signup.html', {'form': form})
